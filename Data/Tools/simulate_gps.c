1888e2e055fae9900133df80da56a05b8d074105
Eric Hazen <ericshazen@gmail.com>
2025-09-29 16:20:30 -0400

    debugging simulation

--- Data/Tools/simulate_gps.c
+++ Data/Tools/simulate_gps.c
@@ -23,20 +23,100 @@
 #include "places.h"
 #include "distance_miles.h"
 
-static char buff[100];
+static char buff[250];
 static char* tokn[10];
 #define MAXT (sizeof(tokn)/sizeof(tokn[0]))
 
-a_place pl;
+static FILE *fp;
+static FILE *fg;
+static FILE *fi;
 
-static uint32_t poffs[100];
-#define MAXPOFF (sizeof(poffs)/sizeof(uint32_t))
-static int noff = 0;
+// structure for an in-memory location
+typedef struct {
+  uint32_t offset;		/* offset in places file */
+  float distance;		/* distance to our GPS location */
+} a_loc;
+
+// array of up to 100 nearby locations from our grid cell
+// and neighboring ones
+static a_loc locs[100];
+#define MAXLOC (sizeof(locs)/sizeof(uint32_t))
+static int numLoc;		/* pointer to last occupied in locs */
+
+// sort by location
+int cmpf( const void* a, const void* b) {
+  a_loc* pa = (a_loc *)a;
+  a_loc* pb = (a_loc *)b;
+  float v1 = pa->distance;
+  float v2 = pb->distance;
+  int rc;
+  if( v1 == v2)
+    rc = 0;
+  else {
+    if( v1 - v2 < 0)
+      rc = -1;
+    else
+      rc = 1;
+  }
+  return rc;
+}
+
+//
+// process a grid 
+int process_grid_at( float lat, float lon, int latGrid, int lonGrid, a_loc* pary, int maxp) {
+
+  // check for out of range
+  if( latGrid < 0 || lonGrid < 0 || lonGrid > 255 || latGrid > 255)
+    return 0;
+
+  int nloc = 0;
+  a_place pl;
+
+  uint32_t posn = ((latGrid << 8) | lonGrid) * sizeof( uint32_t);
+  printf("posn (0x%02x, 0x%02x) = 0x%04x (%d)\n", latGrid, lonGrid, posn, posn);
+  fseek( fi, posn, SEEK_SET);
+
+  uint32_t offset;
+  fread( &offset, 1, sizeof(offset), fi);
+  printf( "Retrieved offset 0x%04x from position %d\n", offset, posn);
+
+  if( offset != 0xffffff) {
+
+    printf("Seeking to offset\n");
+
+    fseek( fp, offset, SEEK_SET);
+    uint32_t offs0 = offset;
+
+    // read until EOF or grid changes
+    while( fgets( buff, sizeof(buff), fp)) {
+      offset = ftell( fp);
+      if( csv_to_place( &pl, buff)) {
+	printf("Error parsing %s\n", buff);
+	exit(1);
+      }
+      if( pl.lat_grid == latGrid && pl.lon_grid == lonGrid) {
+	// save this place with it's distance
+	if( nloc < MAXLOC) {
+	  pary[nloc].offset = offset;
+	  pary[nloc].distance = distance_miles( lat, lon, pl.lat, pl.lon);
+	  ++nloc;
+	}
+	offs0 = offset;
+      } else {
+	break;
+      }
+    }
+  }
+  printf("read %d locations\n", nloc);
+  return nloc;
+}
 
 int main( int argc, char *argv[]) {
-  FILE *fp = fopen( PLACES_FILE, "rb");
-  FILE *fg = fopen( GRID_FILE, "rb");
-  FILE *fi = fopen( INDEX_FILE, "rb");
+  a_place pl;
+
+  fp = fopen( PLACES_FILE, "rb");
+  fg = fopen( GRID_FILE, "rb");
+  fi = fopen( INDEX_FILE, "rb");
   if( !fp || !fg || !fi) {
     fprintf( stderr, "Failed to open a required file\n");
     exit(1);
@@ -59,6 +139,7 @@ int main( int argc, char *argv[]) {
     exit(1);
   }
 
+  // get grid specifications from file
   int numLat = atoi( tokn[0]);
   float latMin = atof( tokn[1]);
   float latMax = atof( tokn[2]);
@@ -68,49 +149,43 @@ int main( int argc, char *argv[]) {
   float lonMax = atof( tokn[6]);
   float lonStep = atof( tokn[7]);
 
-  int latGrid = (int)((lat - latMin) / latStep);
-  int lonGrid = (int)((lon - lonMin) / lonStep);
+  // calculate our grid location from "GPS"
+  int latGrid = (lat - latMin) / latStep;
+  int lonGrid = (lon - lonMin) / lonStep;
+
+  // calculate remainder (offset within grid cell)
+  float lat_rem = lat - (latMin + (float)latGrid * latStep);
+  float lon_rem = lon - (lonMin + (float)lonGrid * lonStep);
+
+  // calculate which direction to look in neighbor cells
+  int latNeighbor = lat_rem > 0.5 ? latGrid+1 : latGrid-1;
+  int lonNeighbor = lon_rem > 0.5 ? lonGrid+1 : lonGrid-1;
 
   printf("Grid: Lat %d %f .. %f step %f \n", numLat, latMin, latMax, latStep);
   printf("Grid: Lon %d %f .. %f step %f \n", numLon, lonMin, lonMax, lonStep);
   printf("Grid for lat %f lon %f is (%d, %d)\n", lat, lon, latGrid, lonGrid);
+  printf("Neighbors: %d, %d\n", latNeighbor, lonNeighbor);
 
-  // gridIndex[tp.lat_grid][tp.lon_grid] = posn;
-  uint32_t posn = ((latGrid << 8) | lonGrid) * sizeof( uint32_t);
-  printf("posn (0x%02x, 0x%02x) = 0x%04x (%d)\n", latGrid, lonGrid, posn, posn);
-  fseek( fi, posn, SEEK_SET);
+  // process 4 neighboring grids
+  numLoc = 0;
+  printf("numLoc = %d\n", numLoc);
+  numLoc += process_grid_at( lat, lon, latGrid, lonGrid, &locs[numLoc], MAXLOC-numLoc);
+  printf("numLoc = %d\n", numLoc);
+  numLoc += process_grid_at( lat, lon, latNeighbor, lonGrid, &locs[numLoc], MAXLOC-numLoc);
+  printf("numLoc = %d\n", numLoc);
+  numLoc += process_grid_at( lat, lon, latGrid, lonNeighbor, &locs[numLoc], MAXLOC-numLoc);
+  printf("numLoc = %d\n", numLoc);
+  numLoc += process_grid_at( lat, lon, latNeighbor, lonNeighbor, &locs[numLoc], MAXLOC-numLoc);
+  printf("numLoc = %d\n", numLoc);
 
-  uint32_t offset;
-  fread( &offset, 1, sizeof(offset), fi);
-  printf( "Retrieved offset 0x%04x from position %d\n", offset, posn);
+  printf("%d places in grid squares\n", numLoc);
+  qsort( locs, numLoc, sizeof(locs[0]), cmpf);
 
-  fseek( fp, offset, SEEK_SET);
-  uint32_t offs0 = offset;
-
-  // read until EOF or grid changes
-  while( fgets( buff, sizeof(buff), fp)) {
-    offset = ftell( fp);
-    if( csv_to_place( &pl, buff)) {
-      printf("Error parsing %s\n", buff);
-      exit(1);
-    }
-    if( pl.lat_grid == latGrid && pl.lon_grid == lonGrid) {
-      // save this place with it's distance
-      if( noff < MAXPOFF)
-	poffs[noff++] = offs0;
-      offs0 = offset;
-    } else {
-      break;
-    }
-  }
-
-  printf("%d places in grid square\n", noff);
-  for( int i=0; i<noff; i++) {
-    fseek( fp, poffs[i], SEEK_SET);
+  for( int i=0; i<numLoc; i++) {
+    printf("offset = %d  dist = %f : ", locs[i].offset, locs[i].distance);
+    fseek( fp, locs[i].offset, SEEK_SET);
     fgets( buff, sizeof(buff), fp);
     csv_to_place( &pl, buff);
     dump_place( &pl);
-    float dist = distance_miles( lat, lon, pl.lat, pl.lon);
-    printf("dist = %f mi\n", dist);
   }
 }
